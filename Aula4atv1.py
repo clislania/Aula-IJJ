510e7375d024847d0bda33f10eb720b217601267
Clislania <clislaniabs@gmail.com>
2023-09-05 21:25:46 -0300

    Commit aula 4 atv1

---
+++ Aula4atv1.py
@@ -0,0 +1,7 @@
+'''
+Solicitar ao usuário a idade humana do pet (um número inteiro).
+Calcular a idade do pet, levando em consideração que cada ano da idade humana corresponde a 7.
+Exibir a idade do pet ao usuário.
+'''
+idade = int(input("Digite a idade humana seu pet: "))
+print("A idade do seu pet é: " + str(idade*7))
